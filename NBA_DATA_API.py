4788686a2f997b0aaaf1141b751c4f5bd0f87cca
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-09 21:56:06 -0300

    Continuation of the project

--- NBA_DATA_API.py
+++ NBA_DATA_API.py
@@ -5,4 +5,21 @@ ALL_JSON = "/prod/v1/today.json"
 
 
 def get_links():
-    respomse =
+    response = get(BASE_URL+ALL_JSON).json()
+    return response["links"]
+
+
+def get_currentScoreboard():
+    response = get(BASE_URL+get_links()["currentScoreboard"]).json()
+    games = response["games"]
+
+    for game in games:
+        home_team = game["hTeam"]
+        away_team = game["vTeam"]
+        clock = game["clock"]
+        period = game["period"]
+
+        print("#########################################\n")
+        print(f"{home_team['triCode']} vs {away_team['triCode']}")
+        print(f"SCORE: {home_team['score']} x {away_team['score']}")
+        print(f"{clock} - {period['current']}\n")
