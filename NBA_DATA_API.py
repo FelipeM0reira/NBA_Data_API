99c621aba8ccf32af6cf9a710f4ebc1d9ccc2bfc
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-11 21:15:08 -0300

    project ready

--- NBA_DATA_API.py
+++ NBA_DATA_API.py
@@ -18,8 +18,43 @@ def get_currentScoreboard():
         away_team = game["vTeam"]
         clock = game["clock"]
         period = game["period"]
+        arena = game["arena"]
 
         print("#########################################\n")
+        print(
+            f"Arena: {arena['name']} City: {arena['city']} Date: {game['startDateEastern']} ")
         print(f"{home_team['triCode']} vs {away_team['triCode']}")
         print(f"SCORE: {home_team['score']} x {away_team['score']}")
         print(f"{clock} - {period['current']}\n")
+
+
+def get_teams_stats():
+    stats = get_links()["leagueTeamStatsLeaders"]
+    data = get(BASE_URL+stats).json()
+    teams = data["league"]["standard"]["regularSeason"]["teams"]
+
+    teams = list(filter(lambda x: x["name"] != "Team", teams))
+    teams.sort(key=lambda x: int(x["ppg"]["rank"]))
+
+    print("Regular Season")
+    for team in teams:
+        team_name = team["name"]
+        nickname = team["nickname"]
+        ppg_avg = team["ppg"]["avg"]
+        rank = team["ppg"]["rank"]
+        print(f"RANK: {rank} | {team_name} - {nickname} | PPG Médio: {ppg_avg}")
+
+
+while True:
+    print("########################\n")
+    print("SEJA BEM VINDO!!!! DADOS NBA IURYROSALTECH\n")
+    print("1 - Ver Jogos\n")
+    print("2 - Ver Times por PPG\n")
+    user_choice = input("Opção: ")
+
+    if user_choice == "1":
+        get_currentScoreboard()
+    elif user_choice == "2":
+        get_teams_stats()
+    else:
+        continue
